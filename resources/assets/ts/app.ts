a6ffe0376e7f39e64543897ae62a5e786765c172
LoicOuth <loic.outhier.work@gmail.com>
2024-06-03 01:38:01 +0200

    feat(navigation): Closed navigation burger menu when change route

--- resources/assets/ts/app.ts
+++ resources/assets/ts/app.ts
@@ -3,6 +3,8 @@ import './vendors/vite.js'
 import './vendors/fontawesome_icons.js'
 import './components/index.js'
 
+declare var up: any
+
 const burgerBtn = document.getElementById('burger-btn')
 const burgerMenu = document.getElementById('burger-menu')
 
@@ -15,5 +17,13 @@ burgerBtn.addEventListener('click', () => {
     burgerMenu.classList.add('show')
     burgerBtn.classList.remove('fa-bars')
     burgerBtn.classList.add('fa-xmark')
+  }
+})
+
+up.on('up:location:changed', () => {
+  if (burgerMenu.classList.contains('show')) {
+    burgerMenu.classList.remove('show')
+    burgerBtn.classList.add('fa-bars')
+    burgerBtn.classList.remove('fa-xmark')
   }
 })
