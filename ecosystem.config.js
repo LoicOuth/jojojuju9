c9512e6367b665dd307ec5c780bc747076446aed
LoicOuth <loic.outhier.work@gmail.com>
2024-07-23 11:55:19 +0200

    fix(deployment): modified ecosystem & fix .env modify step

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,12 +3,10 @@ module.exports = {
     {
       name: 'jojojuju9_webserver',
       script: './bin/server.js',
-      autorestart: true,
     },
     {
       name: 'jojojuju9_scheduler',
       script: 'node ace scheduler:run',
-      autorestart: true,
     },
   ],
 }
