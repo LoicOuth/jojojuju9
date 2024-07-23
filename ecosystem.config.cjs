eaab25bdd62b47b29992943fd5e3139e9de0de55
LoicOuth <loic.outhier.work@gmail.com>
2024-07-23 11:57:36 +0200

    fix(deployment): modified ecosystem to common js file

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,10 +3,12 @@ module.exports = {
     {
       name: 'jojojuju9_webserver',
       script: './bin/server.js',
+      autorestart: true,
     },
     {
       name: 'jojojuju9_scheduler',
       script: 'node ace scheduler:run',
+      autorestart: true,
     },
   ],
 }
